d40d6ccbacde64c5aa5a9ed33274ed7b0f110429
Steven Buczkowski <sbuczko1@umbc.edu>
2022-10-21 23:28:01 -0400

    Skeleton copied over from REPL history

--- src/EarthData.jl
+++ src/EarthData.jl
@@ -2,9 +2,13 @@ module EarthData
 
 # Write your package code here.
 using LibCURL
+using JSON
+using DateTime
+using TimeZones
 
 netrcpath="/Users/steven/.netrc"
 cookiefile="/Users/steven/.urs_cookies"
+
 
 curl = curl_easy_init()
 curl_easy_setopt(curl, CURLOPT, "hhtps://data.gesdisc.earthdata.nasa.gov/s3credentials")
@@ -13,6 +17,17 @@ curl_easy_setopt(curl, CURLOPT_NETRC_FILE, netrcpath)
 curl_easy_setopt(curl, CURLOPT_COOKIEFILE, cookiefile)
 curl_easy_setopt(curl, CURLOPT_COOKIEJAR, cookiefile)
 curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1)
+curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, @cfunction(curl_write_cb, Csize_t, (Ptr{Cvoid}, Csize_t, Csize_t, Ptr{Cvoid})) )
+buffer = UInt8[]
+curl_easy_setopt(curl, CURLOPT_WRITEDATA, pointer_from_objref(buffer))
+
+GC.@preserve buffer begin
+    res = curl_easy_perform(curl)
+end
+
+j = JSON.parse(String(buffer))
+
+aws.credentials.expiry = DateTime(ZonedDateTime(j["expiration"], "yyyy-mm-dd HH:MM:SSzzzz"))
 
 function curl_write_cb(curlbuf::Ptr{Cvoid}, s::Csize_t, p_ctxt::Ptr{Cvoid})::Csize_t
     sz = s * n
