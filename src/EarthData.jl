31984af7e057e67781b37bca688337b7d3f26522
Steven Buczkowski <sbuczko1@umbc.edu>
2022-10-27 11:46:45 -0400

    Continuing to work toward a functioning package. NOT THERE YET

--- src/EarthData.jl
+++ src/EarthData.jl
@@ -3,31 +3,8 @@ module EarthData
 # Write your package code here.
 using LibCURL
 using JSON
-using DateTime
 using TimeZones
-
-netrcpath="/Users/steven/.netrc"
-cookiefile="/Users/steven/.urs_cookies"
-
-
-curl = curl_easy_init()
-curl_easy_setopt(curl, CURLOPT, "hhtps://data.gesdisc.earthdata.nasa.gov/s3credentials")
-curl_easy_setopt(curl, CURLOPT_NETRC, CURL_NETRC_REQUIRED)
-curl_easy_setopt(curl, CURLOPT_NETRC_FILE, netrcpath)
-curl_easy_setopt(curl, CURLOPT_COOKIEFILE, cookiefile)
-curl_easy_setopt(curl, CURLOPT_COOKIEJAR, cookiefile)
-curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1)
-curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, @cfunction(curl_write_cb, Csize_t, (Ptr{Cvoid}, Csize_t, Csize_t, Ptr{Cvoid})) )
-buffer = UInt8[]
-curl_easy_setopt(curl, CURLOPT_WRITEDATA, pointer_from_objref(buffer))
-
-GC.@preserve buffer begin
-    res = curl_easy_perform(curl)
-end
-
-j = JSON.parse(String(buffer))
-
-aws.credentials.expiry = DateTime(ZonedDateTime(j["expiration"], "yyyy-mm-dd HH:MM:SSzzzz"))
+using AWS
 
 function curl_write_cb(curlbuf::Ptr{Cvoid}, s::Csize_t, p_ctxt::Ptr{Cvoid})::Csize_t
     sz = s * n
@@ -43,5 +20,33 @@ function curl_write_cb(curlbuf::Ptr{Cvoid}, s::Csize_t, p_ctxt::Ptr{Cvoid})::Csi
 
     append!(j_ctxt, data)
 end
+
+function earthdata_get_s3credentials(fn::Function, aws::AWSCredentials)::AWSCredentials
+netrcpath="/Users/steven/.netrc"
+cookiefile="/Users/steven/.urs_cookies"
+
+
+curl = curl_easy_init()
+curl_easy_setopt(curl, CURLOPT_URL, "https://data.gesdisc.earthdata.nasa.gov/s3credentials")
+curl_easy_setopt(curl, CURLOPT_NETRC, CURL_NETRC_REQUIRED)
+curl_easy_setopt(curl, CURLOPT_NETRC_FILE, netrcpath)
+curl_easy_setopt(curl, CURLOPT_COOKIEFILE, cookiefile)
+curl_easy_setopt(curl, CURLOPT_COOKIEJAR, cookiefile)
+curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1)
+curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, @cfunction(fn, Csize_t, (Ptr{Cvoid}, Csize_t, Csize_t, Ptr{Cvoid})) )
+buffer = UInt8[]
+curl_easy_setopt(curl, CURLOPT_WRITEDATA, pointer_from_objref(buffer))
+
+GC.@preserve buffer begin
+    res = curl_easy_perform(curl)
+end
+
+println(String(buffer))
+j = JSON.parse(String(buffer))
+
+end
+
+# aws.credentials.expiry = DateTime(ZonedDateTime(j["expiration"], "yyyy-mm-dd HH:MM:SSzzzz"))
+
 
 end
