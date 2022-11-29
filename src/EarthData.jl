6b21eeff04dba0c7d872d3af945e1ec20d8022ef
Steven Buczkowski <sbuczko1@umbc.edu>
2022-11-28 22:33:53 -0500

    Tied together credential struct output

--- src/EarthData.jl
+++ src/EarthData.jl
@@ -1,12 +1,53 @@
 module EarthData
 
+export earthdata_get_s3credentials, curl_write_cb
+
 # Write your package code here.
 using LibCURL
 using JSON
 using TimeZones
-using AWS
+using AWS: AWSCredentials
 
-function curl_write_cb(curlbuf::Ptr{Cvoid}, s::Csize_t, p_ctxt::Ptr{Cvoid})::Csize_t
+# earthdata_get_s3credentials is caled with a valid AWSCredential struct which should
+# be built off the user's .aws credential files with global_aws_config() prior to updating 
+# with EarthData creds
+function earthdata_get_s3credentials(aws::AWSCredentials)::AWSCredentials
+
+# Set user Earthdata login info locations
+userhome = ENV["HOME"]
+netrcpath=joinpath(userhome,".netrc")
+cookiefile=joinpath(userhome,".urs_cookies")
+
+# Set up curl options for the Earthdata web query
+curl = curl_easy_init()
+curl_easy_setopt(curl, CURLOPT_URL, "https://data.gesdisc.earthdata.nasa.gov/s3credentials")
+curl_easy_setopt(curl, CURLOPT_NETRC, CURL_NETRC_REQUIRED)
+curl_easy_setopt(curl, CURLOPT_NETRC_FILE, netrcpath)
+curl_easy_setopt(curl, CURLOPT_COOKIEFILE, cookiefile)
+curl_easy_setopt(curl, CURLOPT_COOKIEJAR, cookiefile)
+curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1)
+curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, @cfunction(curl_write_cb, Csize_t, (Ptr{Cvoid}, Csize_t, Csize_t, Ptr{Cvoid})) )
+buffer = UInt8[]
+curl_easy_setopt(curl, CURLOPT_WRITEDATA, pointer_from_objref(buffer))
+
+# post the query and fill buffer with the JSON data as a string
+GC.@preserve buffer begin
+    res = curl_easy_perform(curl)
+end
+
+# parse into formal JSON struct
+j = JSON.parse(String(buffer))
+
+# Replace AWSCredential fields as required with new values from EarthData
+aws.access_key_id = j["access_key_id"]
+aws.secret_key = j["secret_key"] 
+aws.expiry = DateTime(ZonedDateTime(j["expiration"], "yyyy-mm-dd HH:MM:SSzzzz"))
+aws.renew = earthdata_get_s3credentials
+
+return aws
+end
+
+function curl_write_cb(curlbuf::Ptr{Cvoid}, s::Csize_t, n::Csize_t, p_ctxt::Ptr{Cvoid})::Csize_t
     sz = s * n
     data = Array{UInt8}(undef, sz)
 
@@ -19,33 +60,8 @@ function curl_write_cb(curlbuf::Ptr{Cvoid}, s::Csize_t, p_ctxt::Ptr{Cvoid})::Csi
     end
 
     append!(j_ctxt, data)
+    return sz::Csize_t
 end
-
-function earthdata_get_s3credentials(fn::Function, aws::AWSCredentials)::AWSCredentials
-netrcpath="/Users/steven/.netrc"
-cookiefile="/Users/steven/.urs_cookies"
-
-
-curl = curl_easy_init()
-curl_easy_setopt(curl, CURLOPT_URL, "https://data.gesdisc.earthdata.nasa.gov/s3credentials")
-curl_easy_setopt(curl, CURLOPT_NETRC, CURL_NETRC_REQUIRED)
-curl_easy_setopt(curl, CURLOPT_NETRC_FILE, netrcpath)
-curl_easy_setopt(curl, CURLOPT_COOKIEFILE, cookiefile)
-curl_easy_setopt(curl, CURLOPT_COOKIEJAR, cookiefile)
-curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1)
-curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, @cfunction(fn, Csize_t, (Ptr{Cvoid}, Csize_t, Csize_t, Ptr{Cvoid})) )
-buffer = UInt8[]
-curl_easy_setopt(curl, CURLOPT_WRITEDATA, pointer_from_objref(buffer))
-
-GC.@preserve buffer begin
-    res = curl_easy_perform(curl)
-end
-
-println(String(buffer))
-j = JSON.parse(String(buffer))
-
-end
-
 # aws.credentials.expiry = DateTime(ZonedDateTime(j["expiration"], "yyyy-mm-dd HH:MM:SSzzzz"))
 
 
