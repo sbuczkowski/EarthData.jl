04f11e601b4a59a50dc8d0d4058537d79fd3170a
Steven Buczkowski <sbuczko1@umbc.edu>
2023-01-18 12:37:43 -0500

    Still trying to understand julia module issues
    Moving functions around trying to understand namespace games and why
    code works at REPL but not as a module

--- src/EarthData.jl
+++ src/EarthData.jl
@@ -1,6 +1,6 @@
 module EarthData
 
-export earthdata_get_s3credentials, curl_write_cb
+export earthdata_get_s3credentials, awscred
 
 # Write your package code here.
 using LibCURL
@@ -8,10 +8,26 @@ using JSON
 using TimeZones
 using AWS: AWSCredentials
 
+function curl_write_cb(curlbuf::Ptr{Cvoid}, s::Csize_t, n::Csize_t, p_ctxt::Ptr{Cvoid})::Csize_t
+    sz = s * n
+    data = Array{UInt8}(undef, sz)
+    
+    ccall(:memcpy, Ptr{Cvoid}, (Ptr{Cvoid}, Ptr{Cvoid}, UInt64), data, curlbuf, sz)
+    
+    if p_ctxt == C_NULL
+        j_ctxt = nothing
+    else
+        j_ctxt = unsafe_pointer_to_objref(p_ctxt)
+    end
+    
+    append!(j_ctxt, data)
+    return sz::Csize_t
+end
+
 # earthdata_get_s3credentials is caled with a valid AWSCredential struct which should
 # be built off the user's .aws credential files with global_aws_config() prior to updating 
 # with EarthData creds
-function earthdata_get_s3credentials(aws::AWSCredentials)::AWSCredentials
+function earthdata_get_s3credentials()::AWSCredentials
 
 # Set user Earthdata login info locations
 userhome = ENV["HOME"]
@@ -26,7 +42,9 @@ curl_easy_setopt(curl, CURLOPT_NETRC_FILE, netrcpath)
 curl_easy_setopt(curl, CURLOPT_COOKIEFILE, cookiefile)
 curl_easy_setopt(curl, CURLOPT_COOKIEJAR, cookiefile)
 curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1)
-curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, @cfunction(curl_write_cb, Csize_t, (Ptr{Cvoid}, Csize_t, Csize_t, Ptr{Cvoid})) )
+
+    c_curl_write_cb = @cfunction(curl_write_cb, Csize_t, (Ptr{Cvoid}, Csize_t, Csize_t, Ptr{Cvoid}))
+curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, c_curl_write_cb)
 buffer = UInt8[]
 curl_easy_setopt(curl, CURLOPT_WRITEDATA, pointer_from_objref(buffer))
 
@@ -36,33 +54,18 @@ GC.@preserve buffer begin
 end
 
 # parse into formal JSON struct
-j = JSON.parse(String(buffer))
 
-# Replace AWSCredential fields as required with new values from EarthData
-aws.access_key_id = j["access_key_id"]
-aws.secret_key = j["secret_key"] 
-aws.expiry = DateTime(ZonedDateTime(j["expiration"], "yyyy-mm-dd HH:MM:SSzzzz"))
-aws.renew = earthdata_get_s3credentials
+    j = JSON.parse(String(buffer))
 
-return aws
+    # Replace AWSCredential fields as required with new values from EarthData
+awscred = AWSCredentials()
+    awscred.access_key_id = j["accessKeyId"]
+awscred.secret_key = j["secretAccessKey"] 
+awscred.expiry = DateTime(ZonedDateTime(j["expiration"], "yyyy-mm-dd HH:MM:SSzzzz"))
+    awscred.token = j["sessionToken"]
+    awscred.renew = earthdata_get_s3credentials
+
+return awscred
 end
-
-function curl_write_cb(curlbuf::Ptr{Cvoid}, s::Csize_t, n::Csize_t, p_ctxt::Ptr{Cvoid})::Csize_t
-    sz = s * n
-    data = Array{UInt8}(undef, sz)
-
-    ccall(:memcpy, Ptr{Cvoid}, (Ptr{Cvoid}, Ptr{Cvoid}, UInt64), data, curlbuf, sz)
-
-    if p_ctxt == C_NULL
-        j_ctxt = nothing
-    else
-        j_ctxt = unsafe_pointer_to_objref(p_ctxt)
-    end
-
-    append!(j_ctxt, data)
-    return sz::Csize_t
-end
-# aws.credentials.expiry = DateTime(ZonedDateTime(j["expiration"], "yyyy-mm-dd HH:MM:SSzzzz"))
-
 
 end
